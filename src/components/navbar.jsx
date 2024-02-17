3873cf9bd61131cd2d6c85b112cbc431ad50bf15
Amey Bagwe <ameybagwe09@gmail.com>
2024-02-18 01:13:45 +0530

    Animations added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,47 +1,93 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import Web3 from 'web3';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Web3 from "web3";
 
 function Navbar({ player }) {
+  const [connected, setConnected] = useState(false);
+  const [connectedAccount, setConnectedAccount] = useState("");
 
-    const [connected, setConnected] = useState(false);
-    const [connectedAccount, setConnectedAccount] = useState('');
+  async function connectWallet() {
+    try {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        console.log("Wallet connected");
+        const accounts = await web3.eth.getAccounts();
+        // connectedAccount = accounts[0];
+        setConnectedAccount(accounts[0]);
+        console.log(connectedAccount);
+        setConnected(true);
+      }
+    } catch (error) {
+      console.error("Error connecting wallet:", error);
+    }
+  }
 
-    async function connectWallet() {
-        try {
-            if (window.ethereum) {
-                const web3 = new Web3(window.ethereum);
-                await window.ethereum.request({ method: 'eth_requestAccounts' });
-                console.log("Wallet connected");
-                const accounts = await web3.eth.getAccounts()
-                // connectedAccount = accounts[0];
-                setConnectedAccount(accounts[0]);
-                console.log(connectedAccount);
-                setConnected(true);
-            }
-        } catch (error) {
-            console.error("Error connecting wallet:", error);
-        }
-    }    
-
-    return (
-        <nav class="navbar bg-body-tertiary">
-            <div class="container-fluid">
-                <NavLink to="/" style={{ textDecoration: 'none' }}>
-                    <h1 className='text-body-secondary'>Spoti.Fi</h1>
-                </NavLink>
-                <form class="d-flex" role="search">
-                {!connected && <button class="btn btn-success m-2" type="button" onClick={connectWallet}>Connect wallet</button>}
-                    {!player && <NavLink to="mintNFT">
-                        <button class="btn btn-info m-2" type="button" >Publish your song</button>
-                    </NavLink>}
-                </form>
+  return (
+    <nav style={{ backgroundColor: "#581845", height: 100 }} class="navbar ">
+      <div class="container-fluid">
+        <p class="d-inline-flex gap-1">
+          <a
+            style={{ color: "#DAF7A6" }}
+            class="btn "
+            data-bs-toggle="collapse"
+            href="#collapseExample"
+            role="button"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            Connect Your Account
+          </a>
+        </p>
+        {connectedAccount && (
+          <div class="collapse" id="collapseExample">
+            <div style={{ color: "white" }} class="card card-body">
+              {connectedAccount}
             </div>
-            <p className='lead m-2'> <small className='text-body-secondary'>Connected account: </small><strong className='connected-account'>{connectedAccount}</strong></p>
-        </nav>
-    )
+          </div>
+        )}
+
+        <NavLink to="/" style={{ textDecoration: "none" }}>
+          <h1
+            style={{
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            Spoti.Fi
+          </h1>
+        </NavLink>
+
+        <form class="d-flex" role="search">
+          {!connected && (
+            <button
+              style={{ color: "#DAF7A6" }}
+              class="btn m-2"
+              type="button"
+              onClick={connectWallet}
+            >
+              Connect wallet
+            </button>
+          )}
+          {!player && (
+            <NavLink to="mintNFT">
+              <button
+                style={{ color: "#DAF7A6" }}
+                class="btn m-2"
+                type="button"
+              >
+                Publish your song
+              </button>
+            </NavLink>
+          )}
+        </form>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/mintNFT.js
+++ src/pages/mintNFT.js
@@ -1,171 +1,229 @@
-import React from 'react'
-import axios from 'axios';
-import { useState } from 'react';
-import db,{storage} from '../firebase.js'
-import key from '../key.json'
-import { useEffect } from 'react';
-import './mintNFT.css';
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import db, { storage } from "../firebase.js";
+import key from "../key.json";
+import { useEffect } from "react";
+import "./mintNFT.css";
 
 import {
-    getDownloadURL,
-    ref as storageRef,
-    uploadBytes,
+  getDownloadURL,
+  ref as storageRef,
+  uploadBytes,
 } from "firebase/storage";
 
 function MintNFT({ nfts, setNfts, connectedAccount }) {
-    var url1;
-    const [nftFile, setNftFile] = useState('');
-    const [nftThumbnail, setNftThumbnail] = useState('');
-    const [songId, setSongId] = useState(1);
+  var url1;
+  const [nftFile, setNftFile] = useState("");
+  const [nftThumbnail, setNftThumbnail] = useState("");
+  const [songId, setSongId] = useState(1);
 
-    useEffect(() => {
-        console.log(nfts);
-    }, [nfts]);
+  useEffect(() => {
+    console.log(nfts);
+  }, [nfts]);
 
-    useEffect(() => {
-        console.log("NFT file: ", nftFile);
-        console.log("NFT thumbnail: ", nftThumbnail);
-        // if(nftFile.type){
-        //     if(nftFile.type.startsWith('audio/')){
-        //         // newNFT.mediaType = 'audio';
-        //         console.log("audio file");
-        //     } else if (nftFile.type.startsWith('video/')){
-        //         // newNFT.mediaType = 'video';
-        //         console.log("video file");
-        //     } else {
-        //         alert("Invalid file type! Please upload an audio or video file.");
-        //     }
-        // }
-        // const thumbnail = `../thumbnails/${(nfts.length%7) + 1}.jpg`
-        // console.log(thumbnail);
-    }, [nftFile, nftThumbnail]);
+  useEffect(() => {
+    console.log("NFT file: ", nftFile);
+    console.log("NFT thumbnail: ", nftThumbnail);
+    // if(nftFile.type){
+    //     if(nftFile.type.startsWith('audio/')){
+    //         // newNFT.mediaType = 'audio';
+    //         console.log("audio file");
+    //     } else if (nftFile.type.startsWith('video/')){
+    //         // newNFT.mediaType = 'video';
+    //         console.log("video file");
+    //     } else {
+    //         alert("Invalid file type! Please upload an audio or video file.");
+    //     }
+    // }
+    // const thumbnail = `../thumbnails/${(nfts.length%7) + 1}.jpg`
+    // console.log(thumbnail);
+  }, [nftFile, nftThumbnail]);
 
-    const handleUpload = async (e) => {
-        const nftName = document.getElementById('nft-name').value;
-        const nftDescription = document.getElementById('nft-description').value;
-        const nftId = songId;
-        const imageFile = document.getElementById('nft-image').files[0];
-        const imageRef = storageRef(storage, `img/}`);
-        uploadBytes(imageRef, imageFile)
-    .then((snapshot) => {
+  const handleUpload = async (e) => {
+    const nftName = document.getElementById("nft-name").value;
+    const nftDescription = document.getElementById("nft-description").value;
+    const nftId = songId;
+    const imageFile = document.getElementById("nft-image").files[0];
+    const imageRef = storageRef(storage, `img/}`);
+    uploadBytes(imageRef, imageFile)
+      .then((snapshot) => {
         getDownloadURL(snapshot.ref)
-        .then((url) => {
-            console.log(url)
+          .then((url) => {
+            console.log(url);
             url1 = url;
-            
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setSongId(songId + 1);
+    if (!(nftName && nftDescription && nftThumbnail && nftFile)) {
+      alert("Fill all the fields");
+      return;
+    }
+    if (
+      !(
+        nftFile.type.startsWith("audio/") &&
+        nftThumbnail.type.startsWith("image/")
+      )
+    ) {
+      alert("Check the uploaded file types");
+      return;
+    }
+    console.log("Uploading to IPFS...");
+    e.preventDefault();
+    try {
+      const fileData = new FormData();
+      fileData.append("file", nftFile);
+
+      const fileThumbnail = new FormData();
+      fileThumbnail.append("file", nftThumbnail);
+
+      const res = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data: fileData,
+        headers: {
+          pinata_api_key: key.API_Key,
+          pinata_secret_api_key: key.API_Secret,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      // https://cyan-magnetic-rat-616.mypinata.cloud/ipfs/Qmc1fus882uTZhJpvsY4xVYRMUV5NAAaqboNeJRomRtaBY
+      // console.log(res);
+      const fileUrl =
+        "https://cyan-magnetic-rat-616.mypinata.cloud/ipfs/" +
+        res.data.IpfsHash;
+      // console.log(fileUrl);
+      const nftUrl = fileUrl;
+
+      const thum = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data: fileThumbnail,
+        headers: {
+          pinata_api_key: key.API_Key,
+          pinata_secret_api_key: key.API_Secret,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      const thumUrl =
+        "https://cyan-magnetic-rat-616.mypinata.cloud/ipfs/" +
+        thum.data.IpfsHash;
+      const nftOwner = connectedAccount;
+
+      const newNFT = {
+        nftId,
+        nftName,
+        nftDescription,
+        thumUrl,
+        nftUrl,
+        nftOwner,
+      };
+      setNfts([...nfts, newNFT]);
+      console.log("NFTS: ", nfts);
+      db.collection("music")
+        .add({
+          nftDescription: nftDescription,
+          nftName: nftName,
+          nftUrl: nftUrl,
+          nftOwner: nftOwner,
+          thumbnailUrl: url1,
+        })
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
         })
         .catch((error) => {
-            console.log(error.message);
+          console.error("Error adding document: ", error);
         });
-    })
-    .catch((error) => {
-    console.log(error);
-    });
-        setSongId(songId+1);
-        if(!(nftName && nftDescription &&  nftThumbnail && nftFile)) {
-            alert("Fill all the fields")
-            return;
-        }
-        if(!(nftFile.type.startsWith('audio/') && nftThumbnail.type.startsWith('image/'))){
-            alert("Check the uploaded file types");
-            return;
-        }
-        console.log("Uploading to IPFS...");
-        e.preventDefault();
-        try {
-            const fileData = new FormData();
-            fileData.append("file", nftFile);
-
-            const fileThumbnail = new FormData();
-            fileThumbnail.append("file", nftThumbnail);
-
-            const res = await axios({
-                method: "post",
-                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-                data: fileData,
-                headers: {
-                    pinata_api_key: key.API_Key,
-                    pinata_secret_api_key: key.API_Secret,
-                    "Content-Type": "multipart/form-data"
-                }
-            });
-            // https://cyan-magnetic-rat-616.mypinata.cloud/ipfs/Qmc1fus882uTZhJpvsY4xVYRMUV5NAAaqboNeJRomRtaBY
-            // console.log(res);
-            const fileUrl = "https://cyan-magnetic-rat-616.mypinata.cloud/ipfs/" + res.data.IpfsHash;
-            // console.log(fileUrl);
-            const nftUrl = fileUrl;
-
-            const thum = await axios({
-                method: "post",
-                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-                data: fileThumbnail,
-                headers: {
-                    pinata_api_key: key.API_Key,
-                    pinata_secret_api_key: key.API_Secret,
-                    "Content-Type": "multipart/form-data"
-                }
-            });
-            const thumUrl = "https://cyan-magnetic-rat-616.mypinata.cloud/ipfs/" + thum.data.IpfsHash;
-            const nftOwner = connectedAccount;
-
-            const newNFT = { nftId, nftName, nftDescription, thumUrl, nftUrl, nftOwner }
-            setNfts([...nfts, newNFT])
-            console.log("NFTS: ", nfts);
-            db.collection('music').add({"nftDescription":nftDescription,
-            "nftName":nftName,"nftUrl":nftUrl,"nftOwner":nftOwner,
-            "thumbnailUrl":url1})
-            .then((docRef) => {
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch((error) => {
-                console.error("Error adding document: ", error);
-            });
-    
-            
-
-        } catch (error) {
-            console.log(error);
-        }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
   return (
-    <div>
-            <div className="mintForm">
-                <div class="row justify-content-center mintForm-l">
-                    <div class="col-sm-8 col-lg-6 col-xl-4 border border-5 rounded shadow p-3 mintForm-l2">
-                        <h3 class="fw-bolder text-center">Publish Your Song</h3>
-                        <hr />
-                        <div class="m-2">
-                            <label htmlFor="nft-name" class="form-label fw-bold text-primary">
-                                Song name
-                            </label>
-                            <input type="text" class="form-control" id="nft-name" placeholder="Autumn Scene"
-                            />
-                        </div>
-                        <div class="m-2 mt-3">
-                            <label htmlFor="nft-description" class="form-label fw-bold text-primary">Song Description</label>
-                            <textarea class="form-control" id="nft-description" rows="3" placeholder="#soothing #nature #autumn"></textarea>
-                        </div>
-                        <div className='m-2 mt-3'>
-                            <label htmlFor="nft-image" class="form-label">
-                                Upload thumbnail
-                            </label>
-                            <input class="form-control" type="file" accept="image/*" id="nft-image"  />
-                        </div> 
-                        <div className='m-2 mt-3'>
-                            <label htmlFor="nft-file" class="form-label fw-bold text-primary">
-                                Upload Song file (.mp3)
-                            </label>
-                            <input class="form-control" type="file" id="nft-file" accept="audio/*" onChange={(e) => { setNftFile(e.target.files[0]) }} />
-                        </div>
-                        <div class="mt-4 mb-3">
-                            <button class="btn btn-primary mt-3 shadow" type="submit" id="mint-nft" onClick={handleUpload}> Mint my NFT </button>
-                        </div>
-                    </div>
-                </div>
+    <div style={{ backgroundColor: "#900C3F", width: "100%", height: "100%" }}>
+      <div className="mintForm">
+        <div class="row justify-content-center mintForm-l ">
+          <div class="col-sm-8 col-lg-6 col-xl-4 border border-5 rounded shadow p-3 mintForm-l2 mt-5">
+            <h3 class="fw-bolder text-center">Publish Your Song</h3>
+            <hr />
+            <div class="m-2">
+              <label htmlFor="nft-name" class="form-label fw-bold text-primary">
+                Song name
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="nft-name"
+                placeholder="Autumn Scene"
+              />
             </div>
+            <div class="m-2 mt-3">
+              <label
+                htmlFor="nft-description"
+                class="form-label fw-bold text-primary"
+              >
+                Song Description
+              </label>
+              <textarea
+                class="form-control"
+                id="nft-description"
+                rows="3"
+                placeholder="#soothing #nature #autumn"
+              ></textarea>
+            </div>
+            <div className="m-2 mt-3">
+              <label
+                htmlFor="nft-image"
+                class="form-label fw-bold text-primary"
+              >
+                Upload thumbnail
+              </label>
+              <input
+                class="form-control"
+                type="file"
+                accept="image/*"
+                id="nft-image"
+                onChange={(e) => {
+                  setNftThumbnail(e.target.files[0]);
+                }}
+              />
+            </div>
+            <div className="m-2 mt-3">
+              <label htmlFor="nft-file" class="form-label fw-bold text-primary">
+                Upload Song file (.mp3)
+              </label>
+              <input
+                class="form-control"
+                type="file"
+                id="nft-file"
+                accept="audio/*"
+                onChange={(e) => {
+                  setNftFile(e.target.files[0]);
+                }}
+              />
+            </div>
+            <div class="mt-4 mb-3">
+              <button
+                class="btn btn-primary mt-3 shadow"
+                type="submit"
+                id="mint-nft"
+                onClick={handleUpload}
+              >
+                {" "}
+                Mint my NFT{" "}
+              </button>
+            </div>
+          </div>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default MintNFT
+export default MintNFT;
